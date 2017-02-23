2eca8848ef642e1aa6e3cd1693c83dd35b22ef97
scq000 <scq000@hotmail.com>
2017-02-23 20:03:15 +0800

    refactor test

--- test/unit/specs/Select.spec.js
+++ test/unit/specs/Select.spec.js
@@ -10,7 +10,7 @@ describe('MySelect Component', () => {
 	let dropdown;
 
 	it('should render correctly', () => {
-		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 　 'test2' }] }, true);
+		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 'test2' }] }, true);
 
 		expect(vm.$el.querySelector('.select-input')).not.be.null;
 		expect(vm.$el.querySelectorAll('.select-content__item').length).to.equal(2);
@@ -18,7 +18,7 @@ describe('MySelect Component', () => {
 	});
 
 	it('should show dropdown when click the select inputbox', () => {
-		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 　 'test2' }] }, true);
+		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 'test2' }] }, true);
 		input = vm.$el.querySelector('.select-input');
 		dropdown = vm.$el.querySelector('.select-content');
 
@@ -31,32 +31,32 @@ describe('MySelect Component', () => {
 	});
 
 	it('should render the selected value to inputbox when click the option item', () => 　{
-		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 　 'test2' }] }, true);
-		
+		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 'test2' }] }, true);
+
 		const inputbox = vm.$el.querySelector('.select-input__box')
 		const testOption1 = vm.$el.querySelector('.select-content__item:nth-child(1)');
 		const testOption2 = vm.$el.querySelector('.select-content__item:nth-child(2)');
-		
+
 		testOption1.click();
 		Vue.nextTick(() => {
 			expect(inputbox.value).to.equal('test1');
 		});
-		
+
 		testOption2.click();
 		Vue.nextTick(() => {
 			expect(inputbox.value).to.equal('test2');
 		});
-		
+
 	});
 
 	it('should hide when click outside of the select dropdown content', () => 　{
-		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 　 'test2' }] }, true);
+		vm = createTest(MySelect, { options: [{ text: 'test', value: 'test' }, { text: 'test2', value: 'test2' }] }, true);
 		input = vm.$el.querySelector('.select-input');
 		dropdown = vm.$el.querySelector('.select-content');
-		
+
 		input.click();
 		document.body.click();
-		
+
 		Vue.nextTick(() => {
 			expect(dropdown.style.display).to.equal('none');
 		});
